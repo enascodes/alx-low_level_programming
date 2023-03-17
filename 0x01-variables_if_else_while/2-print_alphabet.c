106221265836157aafd6ec50f06a1ac4b5051414
enascodes <enasisanai@gmail.com>
2023-03-18 00:23:58 +0100

    u

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - program that prints the alphabet in lowercase, followed by a new line
+ * You can only use the putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z' ; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
