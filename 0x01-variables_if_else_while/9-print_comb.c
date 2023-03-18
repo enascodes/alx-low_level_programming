b241b082430d51c3112687c277449e7b787cdc74
enascodes <enasisanai@gmail.com>
2023-03-18 02:42:12 +0100

    u

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,15 +14,13 @@ int main(void)
 int i;
 for (i = 0 ; i < 10 ; i++)
 {
+putchar(i + '0');
 if (i != 9)
 {
-putchar(i + '0');
 putchar(',');
 putchar(' ');
 }
-else if (i == 9)
-putchar(i + '0');
-putchar(' ');
 }
 return (0);
+ 
 }
