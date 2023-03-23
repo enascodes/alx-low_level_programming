3f43004bb2dba7cd8bfb96f48c5a52f9119a55a5
enascodes <enasisanai@gmail.com>
2023-03-23 23:41:27 +0000

    u

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper - uppercase letters
+ * @c: character to check
+ *
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+
+int main(void)
+{
+	char c;
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+
+}
